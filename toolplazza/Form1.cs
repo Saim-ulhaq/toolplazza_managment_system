68a4f97f539c95cba9adadc3ace5f4931dd4e079
M.Saim-Ulhaq <114234931+Saim-ulhaq@users.noreply.github.com>
2024-01-22 15:39:44 +0500

    Add project files.

---
+++ toolplazza/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace toolplazza
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=WASEEM;Initial Catalog=Tool;Integrated Security=True");
+            string query = "select *FROM tool1 where username='" + textBox1.Text + "' AND password = '" + textBox2.Text + "'";
+            con.Open();
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                this.Hide();
+                Form2 f = new Form2();
+                f.Show();
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("invalid Username or password");
+            }
+        }
+    }
+}
